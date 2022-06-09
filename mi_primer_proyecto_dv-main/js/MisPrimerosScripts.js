c362a78427384216c8af413258931b986b596601
Anthonny Montero <heatkdwow@gmail.com>
2022-06-09 00:15:32 -0500

    JavaScript

--- mi_primer_proyecto_dv-main/js/MisPrimerosScripts.js
+++ mi_primer_proyecto_dv-main/js/MisPrimerosScripts.js
@@ -1,24 +1,32 @@
 function ocultarMostrarImagen() {
-    // Display puede ser None o Block
-    
-    
-    var valorDisplay = document.getElementById("idImagen").style.display;
-    
-    if(valorDisplay=="none"){
-        valorDisplay="block"
-    }else{
-        valorDisplay="none"
-    }
-    document.getElementById("idImagen").style.display=valorDisplay; 
+  // Display puede ser None o Block
+
+  var valorDisplay = document.getElementById("idImagen").style.display;
+
+  if (valorDisplay == "none") {
+    valorDisplay = "block";
+  } else {
+    valorDisplay = "none";
+  }
+  document.getElementById("idImagen").style.display = valorDisplay;
 }
 
-function enciende(){
-    var valorSrc = document.getElementById("idImagenFoco").src; 
-    console.log(valorSrc); 
-    document.getElementById("idImagenFoco").src ="../img/focoEncendido.png";
+function enciende() {
+  var valorSrc = document.getElementById("idImagenFoco").src;
+  console.log(valorSrc);
+  document.getElementById("idImagenFoco").src = "../img/focoEncendido.png";
 }
-function apaga(){
-    var valorSrc = document.getElementById("idImagenFoco").src; 
-    console.log(valorSrc); 
-    document.getElementById("idImagenFoco").src ="../img/focoApagado.png"
+function apaga() {
+  var valorSrc = document.getElementById("idImagenFoco").src;
+  console.log(valorSrc);
+  document.getElementById("idImagenFoco").src = "../img/focoApagado.png";
+}
+function encenderApagar() {
+  var valor = document.getElementById("idImagenFoco").src;
+  if (valor.match("../img/focoApagado.png")) {
+    valor = "../img/focoEncendido.png";
+  } else {
+    valor = "../img/focoApagado.png";
+  }
+  document.getElementById("idImagenFoco").src = valor;
 }
